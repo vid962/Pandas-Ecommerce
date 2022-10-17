485babe8fc42504f71a4297e8447a16603813f27
Davidgit <dwoji@protonmail.com>
2022-10-17 18:14:53 +0200

    Some comments from previous file

--- main.py
+++ main.py
@@ -30,6 +30,49 @@ AMEXclient_purchase = df[(df['CC Provider'] == 'American Express') & (df['Purcha
 # -----------------------> Credit Cards with expiration date > 2025
 CC_expire_in2022 = df[df['CC Exp Date'].apply(lambda exp: exp[3:] == "25")]['CC Exp Date'].count()
 
+# df = pd.read_csv("Salaries.csv")
+# #print(df.head(5))
+#
+# print(df.columns.tolist())
+#
+# # avarage pay from the data, base salary
+# avarage_base_pay = df["BasePay"].mean()
+#
+# # employee with the highest overtime
+# highest_overtime = df['OvertimePay'].max()
+#
+# # accessing to single person salary
+# joseph_salary = df[df['EmployeeName'] == 'JOSEPH DRISCOLL' ]['TotalPayBenefits']
+#
+# # highest salary and persons name
+# highest_salary_total = df["TotalPay"].max()
+# person = df[df['TotalPayBenefits'] == highest_salary_total ]['EmployeeName']
+#
+# # highest salary and persons name / faster way
+# person_2 = df.loc[df['TotalPayBenefits'].idxmax()]
+#
+# # lovest paid person
+# lowest_person = df.loc[df['TotalPayBenefits'].idxmin()]
+#
+# # avarage base pay of all employees per year (2011-2014)
+# avarage_base_year = df.groupby('Year').mean()['BasePay']
+#
+# # unique joy titles
+# unique_job_titles = df['JobTitle'].nunique()
+#
+# # Top 10 most common job titles
+# most_common_jobs = df['JobTitle'].value_counts().head(10)
+#
+# # number of people with 'Chief' in job title
+# def if_chief(title):
+#     if "chief" in title.lower().split():
+#         return True
+#     else:
+#         return False
+#
+# job_title_chief = sum(df["JobTitle"].apply(lambda x: if_chief(x)))
+
+
 
 
 
